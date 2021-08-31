00b47a90b9999fccc17eebb89ce58db62c11ce89
HyperSkys <66301237+HyperSkys@users.noreply.github.com>
2021-08-31 10:51:35 -0600

    Adding files

---
+++ src/main/java/dev/hyperskys/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package dev.hyperskys.calculator;
+
+import dev.hyperskys.calculator.utils.CalculatorUtils;
+
+public class Calculator {
+
+   public static void main(String[] args) {
+       CalculatorUtils.setup("defaultSetup");
+   }
+
+}
