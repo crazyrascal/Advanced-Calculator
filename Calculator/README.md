f81d93b7d22d527342a478f136839cb7ec521fe0
HyperSkys <66301237+HyperSkys@users.noreply.github.com>
2021-08-31 12:22:23 -0600

    v1.1

---
+++ Calculator/README.md
@@ -0,0 +1,22 @@
+**SIMPLE CALCULATOR [1.1]**
+
+Just a simple calculator console application made in a few minutes can do
+multiplication, division, subtraction, addition.
+
+If you have any errors please submit an issue report and send Error Details!
+
+**ERROR CODES:**
+
+500 - You didn't enter a correct number!
+
+501 - You cannot divide by zero!
+
+502 - You didn't enter a correct operator!
+
+404 - Improper setup method!
+
+If you need any help or anything **McDonalds#0001** is my discord!
+
+**UPDATES**
+
+i made it to where you can input decimals, and it'll solve it.
---
+++ Calculator/src/main/java/dev/hyperskys/calculator/utils/CalculatorUtils.java
@@ -0,0 +1,78 @@
+package dev.hyperskys.calculator.utils;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CalculatorUtils {
+
+    public static void setup(String s_m71d9_5){
+
+        // Variables \\
+        char operator;
+        double num1;
+        double num2 = 0;
+        double answer = 0;
+        // Variables \\
+
+        if(s_m71d9_5.equalsIgnoreCase("defaultSetup")){
+            Scanner scanner = new Scanner(System.in); // Creates a scanner
+
+            System.out.println("Please enter first number in equation: ");
+
+            try { // This stops users from putting letters not numbers! ( Error prevention )
+                num1 = scanner.nextDouble(); // Defines what num1 is
+            } catch (InputMismatchException e){
+                System.out.println("Error 500, You didn't enter a correct number!"); // Error code 500
+                return;
+            }
+
+            System.out.println("Please enter second number in equation: ");
+
+            try {
+                num2 = scanner.nextDouble(); // Defines what num2 is
+            } catch (InputMismatchException e){
+                System.out.println("Error 500, You didn't enter a correct number!"); // Error code 501
+            }
+
+            System.out.println("What operation to use: ");
+            operator = scanner.next().charAt(0); // Defines what operator
+
+            if(0 == num2 && operator == '/'){ // Checks if they are dividing by zero
+                System.out.println("Error 501, You cannot divide by zero!");
+                return;
+            }
+
+            switch(operator) { // Switch statement for operators
+
+                case '+':
+                    answer = num1 + num2;
+                    break;
+
+                case '-':
+                    answer = num1 - num2;
+                    break;
+
+                case '*':
+                    answer = num1 * num2;
+                    break;
+
+                case '/':
+                    answer = num1 / num2;
+                    break;
+
+                default: // If they didn't enter a correct operator
+                    System.out.println("Error 502, You didn't enter a correct operator!");
+                    System.out.println("Usages: '-', '+', '/', '*'");
+                    return;
+            }
+
+            System.out.println("-----------------------");
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + answer);
+            System.out.println("-----------------------");
+        } else {
+            System.out.println("Error 404, improper setup method!");
+        }
+
+    }
+    
+}
