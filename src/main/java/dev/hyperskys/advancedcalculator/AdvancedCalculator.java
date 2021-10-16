ae27999df993d287ca14b40feaba2d56d3a30c69
HyperSkys <66301237+HyperSkys@users.noreply.github.com>
2021-10-16 06:58:26 -0500

    Delete src/main/java/dev/hyperskys/advancedcalculator directory

--- src/main/java/dev/hyperskys/advancedcalculator/AdvancedCalculator.java
+++
@@ -1,9 +0,0 @@
-package dev.hyperskys.advancedcalculator;
-
-import dev.hyperskys.advancedcalculator.dak.MainMenu;
-
-public class AdvancedCalculator {
-   public static void main(String[] args) {
-       MainMenu.runMainMenu();
-   }
-}
